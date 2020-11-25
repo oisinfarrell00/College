699cb34336e526b893114e5c60d56ecd6b127a85
oisinfarrell00 <farreloi@tcd.ie>
2020-11-25 11:56:01 +0100

    Threads

---
+++ Third Year/Operating Systems/exercises/ex5/switch.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ucontext.h>
+
+int main(){
+	int done = 0;
+	ucontext_t one;
+	ucontext_t two;
+	
+	getcontext(&one);
+	printf("hello world!\n");
+	if(!done){
+		done=1;
+		swapcontext(&two, &one);
+	}
+	return 0;
+}
